a3be42a268f43c9b5865a95891f1e5e4cca6be2f
István Szombath <istvan.szombath@evosoft.com>
2020-12-17 12:23:28 +0100

    feat: create Queue datastructure

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -26,14 +26,12 @@ namespace ConsoleApp1
 
         public T pop()
         {
+            //ha üres a verem
+            if (top == null) throw new InvalidOperationException();
 
-            if (top != null)
-            {
-                T ertek = top.Ertek;
-                top = top.Kovetkezo;
-                return ertek;
-            }
-            else throw new InvalidOperationException();
+            T ertek = top.Ertek;
+            top = top.Kovetkezo;
+             return ertek;
         }
 
         public void push(T ertek)
@@ -54,25 +52,117 @@ namespace ConsoleApp1
             return builder.ToString();
         }
     }
+
+    class KetiranyuLancoltListaElem<T>
+    {
+        public T Ertek { get; private set; }
+        public KetiranyuLancoltListaElem<T> Kovetkezo { get; set; }
+        public KetiranyuLancoltListaElem<T> Elozo { get; private set; }
+
+        public KetiranyuLancoltListaElem(T ertek, KetiranyuLancoltListaElem<T> kovetkezo, KetiranyuLancoltListaElem<T> elozo)
+        {
+            Ertek = ertek;
+            Kovetkezo = kovetkezo;
+            Elozo = elozo;
+        }
+    }
+
+    class Sor<T>
+    {
+        private KetiranyuLancoltListaElem<T> eleje;
+
+        public Sor()
+        {
+            this.eleje = null;
+        }
+
+        public T pop()
+        {
+            // ha üres a sor
+            if (eleje == null) throw new InvalidOperationException();
+            
+            T ertek = eleje.Ertek;
+            eleje = eleje.Kovetkezo;
+            return ertek;
+        }
+
+        private KetiranyuLancoltListaElem<T> Utolso()
+        {
+            KetiranyuLancoltListaElem<T> utolso = eleje;
+
+            // ha üres a lista vagy az aktuális elem után már nincs elem
+            while (utolso != null && utolso.Kovetkezo != null)
+            {
+                utolso = utolso.Kovetkezo;
+            }
+
+            return utolso;
+        }
+
+        public void push(T ertek)
+        {
+            KetiranyuLancoltListaElem<T> utolso = Utolso();
+            KetiranyuLancoltListaElem<T> elem = new KetiranyuLancoltListaElem<T>(ertek, null, utolso);
+            if (utolso == null)
+            {
+                // üres a sor
+                eleje = elem;
+            } else
+            {
+                // az utolsó az utolsó előtti lesz
+                utolso.Kovetkezo = elem;
+            }
+
+        }
+        public String toString()
+        {
+            StringBuilder builder = new StringBuilder();
+            for (KetiranyuLancoltListaElem<T> elem = eleje; elem != null; elem = elem.Kovetkezo)
+            {
+                builder.Append(elem.Ertek.ToString()).Append(" ");
+
+
+            }
+            return builder.ToString();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Verem");
 
             Verem<int> verem = new Verem<int>();
-            for (int i = 1; i<100 ;i++) {
-                verem.push(i*2);
+            for (int i = 1; i<10 ;i++) {
+                verem.push(i);
             }
 
-            Console.WriteLine(verem.toString());
+            Console.WriteLine("A verem tartalma: " + verem.toString());
+            Console.WriteLine("Most sorban kiszedjük: ");
 
-            for (int i = 1; i < 100; i++)
+            for (int i = 1; i < 10; i++)
             {
                 Console.Write(verem.pop() + " ");
             }
 
-            Console.WriteLine(verem.toString());
+            Console.WriteLine("");
+            Console.WriteLine("Sor");
+
+            Sor<int> sor = new Sor<int>();
+            for (int i = 1; i < 10; i++)
+            {
+                sor.push(i);
+            }
+
+            Console.WriteLine("A sor tartalma: " + sor.toString());
+            Console.WriteLine("Most sorban kiszedjük: ");
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.Write(sor.pop() + " ");
+            }
+
+
         }
     }
 }
